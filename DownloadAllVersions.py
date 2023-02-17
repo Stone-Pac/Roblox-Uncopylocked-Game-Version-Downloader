f70487168ec8039b069780a4d38141e15ac8fc52
Sogon <107663159+Stone-Pac@users.noreply.github.com>
2023-02-17 15:54:21 -0800

    Update DownloadAllVersions.py

--- DownloadAllVersions.py
+++ DownloadAllVersions.py
@@ -5,18 +5,10 @@ import time
 
 while True:
     game_id = input("Enter the Roblox game ID: ")
+    game_name = input("Enter the game name: ")
     latest_version = input("Enter the latest version you want to download: ")
 
-    
-    url = f"https://games.roblox.com/v1/games/multiget-place-details?placeIds={game_id}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        game_name = response.json()["data"][0]["name"]
-    else:
-        print(f"Error: Unable to retrieve game name for game ID {game_id}")
-        game_name = game_id
-
-    
+    # use the game name as the prefix for the downloaded files
     url = f"https://assetdelivery.roblox.com/v1/asset?id={game_id}&version="
     zip_filename = f"{game_name}.zip"
     with zipfile.ZipFile(zip_filename, mode='w') as zip_file:
@@ -30,14 +22,14 @@ while True:
                 file_bytes = io.BytesIO(response.content).getbuffer()
                 filename = f"{game_name}_{version}.rbxlx"
                 zip_file.writestr(filename, file_bytes)
-                file_size = len(response.content) / 1024 / 1024  
+                file_size = len(response.content) / 1024 / 1024  # convert to MB
                 download_time = end_time - start_time
                 print(f"Downloaded {filename} ({file_size:.2f} MB) in {download_time:.2f} seconds.")
             else:
                 print(f"Error: Unable to download version {version}")
         print(f"All versions downloaded and saved to {zip_filename}")
 
-   
+    # ask the user if they want to download another game
     another_game = input("Do you want to download another game? (y/n) ")
     if another_game.lower() != 'y':
         break
